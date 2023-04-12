1ac42355d8e2b955e413e6540d80e8f264158761
ThorunTorunu <120403012+ThorunTorunu@users.noreply.github.com>
2023-04-12 22:29:05 +0300

    Update bts.py

--- bts.py
+++ bts.py
@@ -1,93 +1,38 @@
 import RPi.GPIO as GPIO
-import time 
+import time
 
-GPIO.setmode(GPIO.BOARD)
+# GPIO pin numaralarını ayarlama
+IN1_PIN = 15
+IN2_PIN = 13
+ENA_PIN = 11
 
-ena = 11
-in2 = 13
-in1 = 15
-in3 = 16 
-in4 = 18
-enb = 22
-
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(ena,GPIO.OUT)
-GPIO.setup(enb,GPIO.OUT)
-
-def ileri():
-    print("Motorlar ileri gidiyor")
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
-    GPIO.output(ena,GPIO.HIGH)
-    
-    GPIO.output(in3,GPIO.HIGH)
-    GPIO.output(in4,GPIO.LOW)
-    GPIO.output(enb,GPIO.HIGH) 
-    
+# GPIO pinlerini kullanılabilir hale getirme
 def geri():
-    print("Motorlar geri gidiyor")
-    GPIO.output(in2,GPIO.HIGH)
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(ena,GPIO.HIGH)
-    
-    GPIO.output(in4,GPIO.HIGH)
-    GPIO.output(in3,GPIO.LOW)
-    GPIO.output(enb,GPIO.HIGH) 
-    
-def sol():
-    print("Motorlar saga gidiyor")
-    GPIO.output(in2,GPIO.HIGH)
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(ena,GPIO.HIGH)
-    
-    GPIO.output(in3,GPIO.HIGH)
-    GPIO.output(in4,GPIO.LOW)
-    GPIO.output(enb,GPIO.HIGH) 
-    
-def sag():
-    print("Motorlar sola gidiyor")
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
-    GPIO.output(ena,GPIO.HIGH)
-    
-    GPIO.output(in4,GPIO.HIGH)
-    GPIO.output(in3,GPIO.LOW)
-    GPIO.output(enb,GPIO.HIGH) 
-    
-def dur():
-    print("Motorlar durdu.")
-    GPIO.output(ena,GPIO.LOW)
-    GPIO.output(enb,GPIO.LOW) 
-    
-try:
-    while True:
-        ileri()
-        time.sleep(2)
-        dur()
-        time.sleep(1)
-        
-        geri()
-        time.sleep(2)
-        dur()
-        time.sleep(1)
-        
-        sol()
-        time.sleep(2)
-        dur()
-        time.sleep(1)
-        
-        sag()
-        time.sleep(2)
-        dur()
-        time.sleep(1)
-        
-    
-except:
+    print("Motorlar ileri gidiyor")
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(IN1_PIN, GPIO.OUT)
+    GPIO.setup(IN2_PIN, GPIO.OUT)
+    GPIO.setup(ENA_PIN, GPIO.OUT)
+
+# Motorun ileriye dönmesini sağlama
+    GPIO.output(IN1_PIN, GPIO.LOW)
+    GPIO.output(IN2_PIN, GPIO.HIGH)
+
+# Motoru çalıştırma
+    GPIO.output(ENA_PIN, GPIO.HIGH)
+
+# Başlangıç zamanını kaydetme
+    start_time = time.time()
+
+# 2 saniye boyunca motorun çalışmasını sağlama
+    while time.time() - start_time < 1.5:
+        pass
+
+# Motoru durdurma
+    GPIO.output(ENA_PIN, GPIO.LOW)
+
     GPIO.cleanup()
-    
-    
-    
+
+geri()
+
     
