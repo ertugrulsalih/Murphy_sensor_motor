964627efdb8214c1898e3ce923b0d60005e2543c
ThorunTorunu <120403012+ThorunTorunu@users.noreply.github.com>
2023-04-18 22:21:17 +0300

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-import time
 
 # GPIO pin numaralarını tanımlayın
 TRIG_PIN = 29
@@ -23,23 +22,29 @@ def measure_distance_2():
     GPIO.output(TRIG_PIN_2, False)
 
     # ECHO_PIN_2'deki yüksek seviye süresini ölçün
-    GPIO.wait_for_edge(ECHO_PIN_2, GPIO.RISING)
-    pulse_start = time.time()
-    GPIO.wait_for_edge(ECHO_PIN_2, GPIO.FALLING)
-    pulse_end = time.time()
+    start_time = time.time()
+    timeout = start_time + 0.1
+    pulse_start = None
+    pulse_end = None
 
-    # Mesafeyi hesaplayın
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
+    while GPIO.input(ECHO_PIN_2) == 0 and start_time < timeout:
+        pulse_start = time.time()
 
-    # Mesafeyi ekrana yazdırın
-    print("Mesafe 2: {} cm".format(distance))
+    while GPIO.input(ECHO_PIN_2) == 1 and start_time < timeout:
+        pulse_end = time.time()
+
+    if pulse_start is not None and pulse_end is not None:
+        # Mesafeyi hesaplayın
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+
+        # Mesafeyi ekrana yazdırın
+        print("Mesafe 2: {} cm".format(distance))
 
 # Sonsuz bir döngüde sensörü okuyun ve buzzer'ı kontrol edin
 while True:
     try:
-        measure_distance_and_trigger_buzzer()
         measure_distance_2()
     except KeyboardInterrupt:
         # Ctrl+C ile kesildiğinde GPIO pinlerini temizle ve çık
