e54183728982bc08ca528c6c296a478f458cf626
ertugrulsalih <124378391+ertugrulsalih@users.noreply.github.com>
2023-04-18 22:35:42 +0300

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -1,51 +1,56 @@
 import RPi.GPIO as GPIO
+import time
 
 # GPIO pin numaralarını tanımlayın
-TRIG_PIN = 29
-ECHO_PIN = 31
-BUZZER_PIN = 33
-
-# Yeni sensör pin numaralarını tanımlayın
-TRIG_PIN_2 = 32
-ECHO_PIN_2 = 36
+TRIG_PIN = 5
+ECHO_PIN = 6
+BUZZER_PIN = 25
 
 # BCM modunu kullanarak GPIO'yu yapılandırın
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
-# TRIG_PIN_2'yi çıkış olarak ayarlayın ve ECHO_PIN_2'yi giriş olarak ayarlayın
-GPIO.setup(TRIG_PIN_2, GPIO.OUT)
-GPIO.setup(ECHO_PIN_2, GPIO.IN)
+# TRIG_PIN'i çıkış olarak ayarlayın ve ECHO_PIN'i giriş olarak ayarlayın
+GPIO.setup(TRIG_PIN, GPIO.OUT)
+GPIO.setup(ECHO_PIN, GPIO.IN)
+# BUZZER_PIN'i çıkış olarak ayarlayın
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
-def measure_distance_2():
-    # TRIG_PIN_2'yi yüksek seviyeye ayarlayarak bir ping gönderin
-    GPIO.output(TRIG_PIN_2, True)
-    GPIO.output(TRIG_PIN_2, False)
+# TRIG_PIN'i düşük seviyeye ayarlayarak sensörü başlatın
+GPIO.output(TRIG_PIN, False)
+time.sleep(0.5)
 
-    # ECHO_PIN_2'deki yüksek seviye süresini ölçün
-    start_time = time.time()
-    timeout = start_time + 0.1
-    pulse_start = None
-    pulse_end = None
+# Fonksiyonu tanımlayın
+def measure_distance_and_trigger_buzzer():
+    # TRIG_PIN'i yüksek seviyeye ayarlayarak bir ping gönderin
+    GPIO.output(TRIG_PIN, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG_PIN, False)
 
-    while GPIO.input(ECHO_PIN_2) == 0 and start_time < timeout:
+    # ECHO_PIN'deki yüksek seviye süresini ölçün
+    while GPIO.input(ECHO_PIN) == 0:
         pulse_start = time.time()
-
-    while GPIO.input(ECHO_PIN_2) == 1 and start_time < timeout:
+    while GPIO.input(ECHO_PIN) == 1:
         pulse_end = time.time()
 
-    if pulse_start is not None and pulse_end is not None:
-        # Mesafeyi hesaplayın
-        pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
+    # Mesafeyi hesaplayın
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
 
-        # Mesafeyi ekrana yazdırın
-        print("Mesafe 2: {} cm".format(distance))
+    # Mesafeyi ekrana yazdırın
+    print("Mesafe: {} cm".format(distance))
+
+    # Mesafe 80 cm'den küçükse buzzer'ı çal
+    if distance < 80:
+        GPIO.output(BUZZER_PIN, True)
+    else:
+        GPIO.output(BUZZER_PIN, False)
 
 # Sonsuz bir döngüde sensörü okuyun ve buzzer'ı kontrol edin
 while True:
     try:
-        measure_distance_2()
+        measure_distance_and_trigger_buzzer()
+        time.sleep(1)
     except KeyboardInterrupt:
         # Ctrl+C ile kesildiğinde GPIO pinlerini temizle ve çık
         GPIO.cleanup()
